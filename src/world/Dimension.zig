dce9b4578aaac3cea21e6062e92235540d194a59
Hans Otto Elneff <elneff@pm.me>
2024-12-19 12:31:40 +0200

    remove premature block shit

--- src/world/Dimension.zig
+++
@@ -1,5 +0,0 @@
-chunks: [2][2]?*Chunk,
-
-const std = @import("std");
-const Block = @import("Block.zig");
-const Chunk = @import("Chunk.zig");
--- src/world/world.zig
+++
@@ -1,3 +0,0 @@
-pub const Block = @import("Block.zig");
-pub const Chunk = @import("Chunk.zig");
-pub const Dimension = @import("Dimension.zig");
