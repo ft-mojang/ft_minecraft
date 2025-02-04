677849f0ebf1d6638998c6a1f914991173492a75
Hans Otto Elneff <elneff@pm.me>
2025-02-04 20:28:08 +0200

    tweaky tweak

--- src/vulkan/vulkan.zig
+++ src/vulkan/vulkan.zig
@@ -5,10 +5,15 @@ pub const DeviceDispatch = vk.DeviceWrapper(apis);
 pub const InstanceProxy = vk.InstanceProxy(apis);
 pub const DeviceProxy = vk.DeviceProxy(apis);
 pub const QueueProxy = vk.QueueProxy(apis);
-pub const CommandBufferProxy = vk.CommandBufferProxy(apis);
 
 pub usingnamespace @import("allocator/allocator.zig");
 pub const Context = @import("Context.zig");
+
+pub const app_info: vk.ApplicationInfo = .{
+    .api_version = vk.API_VERSION_1_2,
+    .application_version = 0,
+    .engine_version = 0,
+};
 
 pub const apis: []const vk.ApiInfo = &.{
     vk.features.version_1_0,
@@ -16,11 +21,4 @@ pub const apis: []const vk.ApiInfo = &.{
     vk.features.version_1_2,
     vk.extensions.khr_surface,
     vk.extensions.khr_swapchain,
-};
-
-pub const app_info: vk.ApplicationInfo = .{
-    .api_version = vk.API_VERSION_1_2,
-    .application_version = 0,
-    .engine_version = 0,
-    .p_application_name = "ft_minecraft",
 };
