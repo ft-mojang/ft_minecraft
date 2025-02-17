5e6a14a17cf778de3158ecad7a324cd6fdd94a03
Hans Otto Elneff <elneff@pm.me>
2025-02-17 18:43:57 +0200

    revert to tall chunks + cleanup/fixes

--- src/worldgen.zig
+++ src/worldgen.zig
@@ -4,46 +4,47 @@ const std = @import("std");
 const math = std.math;
 const debug = std.debug;
 
-/// Enum with a variant for each unique block type
+/// Enum variants for all unique block types.
 pub const Block = enum(u8) {
-    /// Valid range of block coordinates
-    pub const Coord = i32;
-
     air,
     stone,
+
+    /// Valid range of block coordinates.
+    pub const Coord = i32;
 };
 
 pub const Chunk = struct {
-    /// Valid range of chunk coordinates
+    blocks: [volume]Block,
+
+    /// Valid range of chunk coordinates.
     pub const Coord = i28;
 
-    /// Side length of a chunk. Chunks are uniform in size (same length on each axis).
-    pub const size = blk: {
+    /// Side length of a chunk on the vertical y axis.
+    pub const size_y = 256;
+
+    /// Side length of a chunk on the horizontal x and z axes.
+    pub const size_xz = blk: {
         const bit_width = @bitSizeOf(Block.Coord) - @bitSizeOf(Coord);
         debug.assert(bit_width > 0);
         break :blk bit_width * bit_width;
     };
-    /// The number of blocks in a chunk
-    pub const volume = size * size * size;
 
-    blocks: [volume]Block,
+    /// The number of blocks in a chunk.
+    pub const volume = size_xz * size_y * size_xz;
 };
-
-pub const world_height_chunks = 16;
-pub const world_height_blocks = Chunk.size * world_height_chunks;
 
 const noise = fastnoise.Noise(f64){};
 
 pub fn generateChunk(chunk_x: Chunk.Coord, chunk_z: Chunk.Coord) Chunk {
     var chunk: Chunk = undefined;
-    for (0..Chunk.size) |x| {
-        for (0..Chunk.size) |z| {
-            const block_x = @as(Block.Coord, chunk_x) * Chunk.size + @as(Block.Coord, @intCast(x));
-            const block_z = @as(Block.Coord, chunk_z) * Chunk.size + @as(Block.Coord, @intCast(z));
-            const height: Block.Coord = @intFromFloat(noise.genNoise2D(@floatFromInt(block_x), @floatFromInt(block_z)) * world_height_chunks);
-            // TODO(Hans): Figure out how we handle "chunk pillars" when chunks are uniform in size.
-            for (0..Chunk.size) |y| {
-                chunk.blocks[(z * Chunk.size + y) * Chunk.size + x] = if (y < height) .stone else .air;
+    for (0..Chunk.size_xz) |x| {
+        for (0..Chunk.size_xz) |z| {
+            const block_x = @as(Block.Coord, chunk_x) * Chunk.size_xz + @as(Block.Coord, @intCast(x));
+            const block_z = @as(Block.Coord, chunk_z) * Chunk.size_xz + @as(Block.Coord, @intCast(z));
+            const noise_sample = noise.genNoise2D(@floatFromInt(block_x), @floatFromInt(block_z));
+            const height = @as(Block.Coord, @intFromFloat(noise_sample * Chunk.size_y)) - 1;
+            for (0..Chunk.size_y) |y| {
+                chunk.blocks[(z * Chunk.size_y + y) * Chunk.size_xz + x] = if (y <= height) .stone else .air;
             }
         }
     }
@@ -51,10 +52,10 @@ pub fn generateChunk(chunk_x: Chunk.Coord, chunk_z: Chunk.Coord) Chunk {
 }
 
 pub fn printChunk(chunk: Chunk) void {
-    for (0..Chunk.size) |y| {
-        for (0..Chunk.size) |x| {
-            for (0..Chunk.size) |z| {
-                const block = chunk.blocks[(z * Chunk.size + y) * Chunk.size + x];
+    for (0..Chunk.size_y) |y| {
+        for (0..Chunk.size_xz) |x| {
+            for (0..Chunk.size_xz) |z| {
+                const block = chunk.blocks[(z * Chunk.size_y + y) * Chunk.size_xz + x];
                 debug.print("{}", .{@intFromEnum(block)});
             }
             debug.print("\n", .{});
