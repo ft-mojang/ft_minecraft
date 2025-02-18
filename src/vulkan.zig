7996ede41cfcaff4a7782b25b040d87096ea1243
Hans Otto Elneff <elneff@pm.me>
2025-02-18 21:46:08 +0200

    fix brokie subset

--- src/vulkan.zig
+++ src/vulkan.zig
@@ -9,7 +9,6 @@ pub const apis: []const vk.ApiInfo = &.{
     vk.features.version_1_1,
     vk.features.version_1_2,
     vk.extensions.khr_portability_enumeration,
-    vk.extensions.khr_portability_subset,
     vk.extensions.khr_surface,
     vk.extensions.khr_swapchain,
     vk.extensions.ext_debug_utils,
